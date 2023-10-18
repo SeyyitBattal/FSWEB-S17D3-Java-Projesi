5007acf5c31a570ea74a3b4eb963fe57eddc5563
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-18 11:52:12 +0300

    Project set up

---
+++ src/main/java/com/zoo/zooOrganisation/ZooOrganisationApplication.java
@@ -0,0 +1,13 @@
+package com.zoo.zooOrganisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooOrganisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooOrganisationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoo/zooOrganisation/ZooOrganisationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoo.zooOrganisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooOrganisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

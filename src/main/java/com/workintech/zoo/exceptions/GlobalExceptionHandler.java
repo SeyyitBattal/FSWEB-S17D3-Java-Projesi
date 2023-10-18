7214d4cc0cc30fbda5eb70f86ca4a111d411c1ee
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-18 17:10:17 +0300

    Kangroos validation done

--- src/main/java/com/workintech/zoo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/workintech/zoo/exceptions/GlobalExceptionHandler.java
@@ -6,11 +6,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler
     public ResponseEntity<KangrooErrorResponse> handleException(KangrooException kangrooException) {
         KangrooErrorResponse response = new KangrooErrorResponse(
                 kangrooException.getStatus().value(), kangrooException.getMessage(), System.currentTimeMillis()
@@ -19,6 +21,7 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, kangrooException.getStatus());
     }
 
+    @ExceptionHandler
     public ResponseEntity<KangrooErrorResponse> handleException(Exception exception) {
         KangrooErrorResponse response = new KangrooErrorResponse(
                 HttpStatus.BAD_REQUEST.value(), exception.getMessage(), System.currentTimeMillis()
